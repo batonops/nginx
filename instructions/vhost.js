055ebfdf592216d54f7d74c2f9503013be1d4a5a
Jared Hanson <jaredhanson@gmail.com>
2013-01-27 15:32:38 -0800

    Initial commit.

---
+++ instructions/vhost.js
@@ -0,0 +1,14 @@
+module.exports = function(options) {
+  var host = options.host
+    , confFile = options.confFile || 'vhost.ejs'
+
+  return function(sys) {
+    var path = '/etc/nginx/sites-available/' + host;
+  
+    this.file(path, {
+      content: this.template(confFile, {
+        host: host
+      })
+    });
+  }
+}
