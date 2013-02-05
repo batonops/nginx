80ab2d7ad26f4ce268740e8c1cb3db871492b6c5
Jared Hanson <jaredhanson@gmail.com>
2013-02-04 20:40:51 -0800

    Make template path relative.

--- directives/vhost.js
+++ directives/vhost.js
@@ -1,6 +1,6 @@
 module.exports = function(options) {
   var host = options.host
-    , confFile = options.confFile || 'vhost.ejs'
+    , confFile = options.confFile || './vhost.ejs'
 
   return function(sys) {
     var path = '/etc/nginx/sites-available/' + host;
